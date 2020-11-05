b332714844e97edb768e84ee95d74d215d62e3a9
liewailun <liewailun@users.noreply.github.com>
2020-11-05 19:21:59 -0300

    Commit inicial
    Commit inicial

---
+++ molabota/src/main/java/com/lwlun/molabota/MolabotaApplication.java
@@ -0,0 +1,13 @@
+package com.lwlun.molabota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolabotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolabotaApplication.class, args);
+	}
+
+}
---
+++ molabota/src/test/java/com/lwlun/molabota/MolabotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwlun.molabota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolabotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
